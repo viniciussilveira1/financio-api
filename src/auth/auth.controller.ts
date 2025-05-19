18fc7219470340123cfdf54f723d477a938487a5
Vinicius Silveira <vinirafel2008@email.com>
2025-05-18 21:45:15 -0300

    feat: englobado auth no projeto todo

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginRequest } from './models/Authentication';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @Public()
   async login(@Body() body: LoginRequest) {
     const user = await this.authService.validateUser(body);
 
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+// main.ts
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/jwt-auth.guard'; // ajuste o path se estiver diferente
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector)); // aqui aplicamos o guard globalmente
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
